b076d1d3bb4c2e90b899aecd099d8afd1f513d39
Harshit <2105891@kiit.ac.in>
2022-09-26 10:59:59 +0530

    fetched backend data to frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Route } from 'react-router-dom'
+import ChatPage from './Components/ChatPage';
+import HomePage from './Components/HomePage';
+
+console.log("app file ran");
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path='/' component={HomePage} />
+      <Route path='/chats' component={ChatPage} />
+      <h1>hello</h1>
     </div>
   );
 }
---
+++ src/Components/HomePage.js
@@ -0,0 +1,10 @@
+// for home page
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div>Home</div>
+    )
+}
+
+export default HomePage
